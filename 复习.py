31dbdb7f8d2f57288021886715c46620e98e9323
selina12345 <2199479643@qq.com>
2018-08-03 17:21:22 +0800

    1

--- 复习.py
+++ 复习.py
@@ -98,3 +98,31 @@ while i < 100:
         r += i
 print(r)
 print("0-100偶数求和 = %d " % r)
+
+
+class User():
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    def describe_user(self):
+        print(self.first_name+self.last_name)
+    def greet_user(self):
+        print("hey,tony.")
+
+class Privileges():
+    def __init__(self):
+        self.privileges = ["can add post", "can delete post","can ban user"]
+
+    def show_privileges(self):
+        for privileges2 in self.privileges:
+            print("If you are an administrator account, you can have the following permissions: ")
+            print("—" + privileges2)
+
+class Admin(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.privileges = Privileges()
+
+user = Admin('Tony','Stark')
+
+user.privileges.show_privileges()
--- 第9章_类.py
+++ 第9章_类.py
@@ -39,8 +39,6 @@ class Dog():
 
 
 print("\n9.1.2  根据类创建实例")
-
-
 class Dog():
     def __init__(self, name, age):
         self.name = name
@@ -632,7 +630,6 @@ class Restaurant():
     def open_restaurant(self):
         print("This restaurant is open for business.")
 
-
 class IceCreamStand(Restaurant):
     def __init__(self, restaurant_name, cuisine_type):
         super().__init__(restaurant_name, cuisine_type)
@@ -644,7 +641,6 @@ class IceCreamStand(Restaurant):
         for flavor in self.flavors:
             print(" --" + flavor)
 
-
 my_restanrant = IceCreamStand("Xiao_LI_Restaurant", "west_food", )
 print("The restaurant's name is " + my_restanrant.restaurant_name.title() + ".")
 print("The restaurant is good at " + my_restanrant.cuisine_type + ".")
@@ -654,53 +650,57 @@ my_restanrant.open_restaurant()
 my_restanrant.describe_icecream()
 
 print("\n9.7 管理员")
-
-
 class User():
     def __init__(self, first_name, last_name, age, country):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.country = country
+        self.first_name2 = first_name
+        self.last_name2 = last_name
+        self.age2 = age
+        self.country2 = country
 
     def describe_user(self):
-        print("Hello," + self.first_name.title() + self.last_name.title() + " ,")
+        print("Hello," + self.first_name2.title() + self.last_name2.title() + " ,")
 
     def greet_user(self):
         print("Nice to meet you! ")
-
 
 class Admin(User):
     def __init__(self, first_name, last_name, age, country):
         super().__init__(first_name, last_name, age, country)
         privilegs = ["can add post", "can delete post", "can be user"]
-        self.privilegs = privilegs
+        self.privilegs2 = privilegs
 
     def show_privilegs(self):
         print("If you are an administrator account, you can have the following permissions: ")
-        for privileg in self.privilegs:
+        for privileg in self.privilegs2:
             print("—" + privileg)
-
 
 all_users = Admin("li", "zhixin", 18, "china")
 all_users.describe_user()
 all_users.greet_user()
 all_users.show_privilegs()
 
-
+print("\n9.8 权限")
+# # 编写一个名为 Privileges 的类，它只有一个属性——privileges，其中
+# # 存储了练习 9-7 所说的字符串列表。将方法 show_privileges()移到这个类中。在 Admin
+# # 类中，将一个 Privileges 实例用作其属性。创建一个 Admin 实例，并使用方法
+# # show_privileges()来显示其权限。
 class Privilegs():
-    def __init__(self, privilegs):
-        self.privilegs = privilegs
+    def __init__(self):
+        self.privilegs2 = ["can add post", "can delete post", "can ban user"]
 
-
-    def show_privilegs2(self):
+    def show_privilegs(self):
         print("If you are an administrator account, you can have the following permissions: ")
+        for privileg in self.privilegs2:
+            print(privileg)
 
-
-class Admin2():
+class Admin():
     def __init__(self, privilegs):
-       self.privilegs = Privilegs(privilegs)
+        self.privilegs2 = Privilegs()
+
+privilegs = Admin("2324")
+privilegs.privilegs2.show_privilegs()
 
 
-privilegs2 = Admin2("can add post")
-privilegs2.privilegs.show_privilegs2()
+
+
+
